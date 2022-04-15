82d8e5bad2e9ce92a1bb45631ce593b5c81de55f
Bronya Vollerei <103723858+a2VxaW5n@users.noreply.github.com>
2022-04-15 19:05:49 +0900

    visualization

--- matching.py
+++ matching.py
@@ -21,7 +21,7 @@ data = pd.read_csv(file,index_col=0)
 
 seed = int(time.time()) # 便利のため実行時の時間をseedとする
 #seed = 114514 # 事後再現用
-print(seed)
+print("SEED:",seed)
 chara = data.index.to_list()
 random.Random(seed).shuffle(chara) # ハンガリアンの被った場合は順番依存なので，順番をランダムシードでシャッフルする
 #print(chara)
@@ -37,8 +37,21 @@ table = pd.DataFrame(index=weapon,columns=[str(seed)])
 for i in range(len(col_ind)):
     table[str(seed)][weapon[col_ind[i]]] = chara[i]
 
-table.to_csv("result.txt",sep=":")
+#table.to_csv("result.txt",sep=":")
 
 ## Print
+print("('*'は未回答者)")
+print("======= 結果 =======")
 for wp in table.index:
-    print(f"{wp}:{table[str(seed)][wp]}")
+    print(f"{wp}: {table[str(seed)][wp]}")
+
+print("======= 効用 =======")
+print(f"=== 個別:")
+y = []
+for wp in table.index:
+    yv = data[wp][table[str(seed)][wp]]
+    y.append(yv)
+    print(f"    {table[str(seed)][wp]}: {yv}")
+print(f"=== 平均: {sum(y)/len(y):.2g}")
+print(f"=== 最大: {max(y)} (計{y.count(max(y))}人)")
+print(f"=== 最小: {min(y)} (計{y.count(min(y))}人)")
